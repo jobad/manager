9c232430c0ecbb0b360d129f2feeab1d03c4d580
badjo <joebenick@gmail.com>
2015-03-30 07:03:20 +0800

    implementing user API

--- app/Bazaarcornerapi/User.php
+++ app/Bazaarcornerapi/User.php
@@ -8,10 +8,28 @@ class User {
 
         $bc_api = new ApiController();
         $login_data = array(
-		     'username' => 'badjo',
-		     'password' => '123456'
+		     'username' => 'bazaarcorner',
+		     'password' => 'bazaarcorner'
 		);
-        var_dump( $bc_api->request('POST','/login',$login_data));    
+		
+		$new_user_details = array(
+             'role_id'    => '1'
+            ,'username'         => 'badjo'
+            ,'password'         => '123456'
+            ,'email'            => 'joebenick@gmail.com'
+            ,'first_name'       => 'Badjo'
+            ,'last_name'        => 'Badiola'
+            ,'address'			=> ''
+            ,'country_id'		=> ''
+            ,'phone'            => '09291614935'
+            ,'birthdate'         => '1983-08-01'            
+            ,'facebook'         => 'https://www.facebook.com/jobad'
+            ,'twitter'          => 'https://twitter.com/jobad'
+            ,'profile_image'    => ''
+            ,'remember_token'   => ''            
+    	);
+		//var_dump($bc_api->request('POST','/user',$new_user_details)); exit;
+        var_dump( $bc_api->request('POST','/login',$login_data));    exit;
         return $order;
     }
 
