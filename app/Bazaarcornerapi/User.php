b39c3ca876dc41aff16123c6c1ab0838415a9711
badjo <joebenick@gmail.com>
2015-04-02 23:34:20 +0800

    update user controller

--- app/Bazaarcornerapi/User.php
+++ app/Bazaarcornerapi/User.php
@@ -8,11 +8,11 @@ class User {
 
         $bc_api = new ApiController();
         $login_data = array(
-		     'username' => 'badjo',
-		     'password' => '123456'
+		     'username' => $username,
+		     'password' => $password
 		);
 		
-		$new_user_details = array(
+		/*$new_user_details = array(
              'role_id'    => '1'
             ,'user_group_id'    => '1'
             ,'username'         => 'badjo'
@@ -30,9 +30,12 @@ class User {
             ,'profile_image'    => ''
             ,'remember_token'   => ''            
     	);
-		//var_dump($bc_api->request('POST','/user',$new_user_details)); exit;
-        var_dump( $bc_api->request('POST','/login',$login_data));    exit;
-        return $order;
+		var_dump($bc_api->request('POST','/user',$new_user_details)); exit;
+            exit;*/
+        $result = $bc_api->request('POST','/login',$login_data);
+
+
+        return $result;
     }
 
 }
--- app/Http/Controllers/UserauthController.php
+++ app/Http/Controllers/UserauthController.php
@@ -14,11 +14,24 @@ class UserauthController extends Controller {
 		
 	}
 
-	public function login() 
+	public function showLogin() 
 	{
-		$test = new User();
+		//$test = new User();
 
-		var_dump($test->bcLogin('1', '2'));
+		//var_dump($test->bcLogin('1', '2'));
 		return  view('login');
+	}
+
+	public function login(Request $request) {
+		$username = $request->input('username');
+		$password = $request->input('password');
+
+		$user = new User();
+		
+		$validateUser = $user->bcLogin($username, $password);
+
+		var_dump($validateUser);
+
+        return 'Friend added!';
 	}
 }
