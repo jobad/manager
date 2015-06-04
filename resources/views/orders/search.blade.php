79c21a6dd2ba558e027d7ed305b21631181666f3
jobad <joebenick@gmail.com>
2015-06-05 01:00:13 +0800

    remove label

--- resources/views/orders/search.blade.php
+++ resources/views/orders/search.blade.php
@@ -16,8 +16,7 @@
     <div class="row">
       <div class="col-xs-12">
         <div class="box">
-          <div class="box-header">
-            <h3 class="box-title">Add User</h3>
+          <div class="box-header">            
           </div><!-- /.box-header -->
           <div class="box-body">
             {!! Form::open(['id'=>'useradd']) !!}
