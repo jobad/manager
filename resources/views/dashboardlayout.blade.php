ee6408f542447088c3278753238ef51e07c761d2
jobad <joebenick@gmail.com>
2015-06-08 23:13:04 +0800

    fixed layout

--- resources/views/dashboardlayout.blade.php
+++ resources/views/dashboardlayout.blade.php
@@ -5,30 +5,30 @@
     <title>BazaarCorner Manager</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <!-- Bootstrap 3.3.2 -->
-    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />    
+    <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />    
     <!-- FontAwesome 4.3.0 -->
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
     <!-- Ionicons 2.0.0 -->
     <link href="http://code.ionicframework.com/ionicons/2.0.0/css/ionicons.min.css" rel="stylesheet" type="text/css" />    
     <!-- Theme style -->
-    <link href="dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css" />
+    <link href="/dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css" />
     <!-- AdminLTE Skins. Choose a skin from the css/skins 
          folder instead of downloading all of them to reduce the load. -->
-    <link href="dist/css/skins/_all-skins.min.css" rel="stylesheet" type="text/css" />
+    <link href="/dist/css/skins/_all-skins.min.css" rel="stylesheet" type="text/css" />
     <!-- iCheck -->
-    <link href="plugins/iCheck/flat/blue.css" rel="stylesheet" type="text/css" />
+    <link href="/plugins/iCheck/flat/blue.css" rel="stylesheet" type="text/css" />
     <!-- Morris chart -->
-    <link href="plugins/morris/morris.css" rel="stylesheet" type="text/css" />
+    <link href="/plugins/morris/morris.css" rel="stylesheet" type="text/css" />
     <!-- jvectormap -->
-    <link href="plugins/jvectormap/jquery-jvectormap-1.2.2.css" rel="stylesheet" type="text/css" />
+    <link href="/plugins/jvectormap/jquery-jvectormap-1.2.2.css" rel="stylesheet" type="text/css" />
     <!-- Date Picker -->
-    <link href="plugins/datepicker/datepicker3.css" rel="stylesheet" type="text/css" />
+    <link href="/plugins/datepicker/datepicker3.css" rel="stylesheet" type="text/css" />
     <!-- Daterange picker -->
-    <link href="plugins/daterangepicker/daterangepicker-bs3.css" rel="stylesheet" type="text/css" />
+    <link href="/plugins/daterangepicker/daterangepicker-bs3.css" rel="stylesheet" type="text/css" />
     <!-- bootstrap wysihtml5 - text editor -->
-    <link href="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css" rel="stylesheet" type="text/css" />
+    <link href="/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css" rel="stylesheet" type="text/css" />
     <!-- manager css -->
-    <link href="css/manager.css" rel="stylesheet" type="text/css" />
+    <link href="/css/manager.css" rel="stylesheet" type="text/css" />
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -101,37 +101,37 @@
           <ul class="sidebar-menu">
             <li class="header">MAIN NAVIGATION</li>
             <li>
-              <a href="order-search">
+              <a href="/order-search">
                 <i class="fa fa-search"></i> <span>Search Order</span>                
               </a>
             </li>
             <li>
-              <a href="most-bought">
+              <a href="/most-bought">
                 <i class="fa fa-bar-chart-o"></i> <span>Most Bought</span>
               </a>
             </li>
             <li>
-              <a href="top-brands">
+              <a href="/top-brands">
                 <i class="fa fa-list"></i> <span>Top Brands</span>
               </a>
             </li>  
             <li>
-              <a href="merchant-list">
+              <a href="/merchant-list">
                 <i class="fa fa-list-alt"></i> <span>Merchant List</span>
               </a>
             </li>           
-           <li class="treeview">
+           <li class="/treeview">
               <a href="#">
                 <i class="fa fa-users"></i> <span>Users</span> <i class="fa fa-angle-left pull-right"></i>
               </a>
-              <ul class="treeview-menu">
-                <li><a href="user-list"><i class="fa fa-circle-o"></i> List Users</a></li>
-                <li><a href="user-add"><i class="fa fa-circle-o"></i> Add User</a></li>
+              <ul class="/treeview-menu">
+                <li><a href="/user-list"><i class="fa fa-circle-o"></i> List Users</a></li>
+                <li><a href="/user-add"><i class="fa fa-circle-o"></i> Add User</a></li>
                 
               </ul>
             </li>  
             <li>
-              <a href="logout">
+              <a href="/logout">
                 <i class="fa fa-sign-out"></i> <span>Sign Out</span>
               </a>
             </li>          
